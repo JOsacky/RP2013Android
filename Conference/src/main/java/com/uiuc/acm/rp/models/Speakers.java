0f00b3188ac747c3836b0e7f168a0e503339ff02
Jonathan <jonathan.osacky@gmail.com>
2013-10-02 20:20:41 -0500

    Create Speakers.java

---
+++ Conference/src/main/java/com/uiuc/acm/rp/models/Speakers.java
@@ -0,0 +1,60 @@
+import java.net.URL;
+
+import android.graphics.Bitmap;
+
+
+public class Speakers {
+
+	private String bio;
+	private URL webUrl;
+	private String role;
+	private String name;
+	private String twitter;
+	private Bitmap image75;
+	private String speakerBio;
+	
+	public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	public URL getWebUrl() {
+		return webUrl;
+	}
+	public void setWebUrl(URL webUrl) {
+		this.webUrl = webUrl;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTwitter() {
+		return twitter;
+	}
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+	public Bitmap getImage75() {
+		return image75;
+	}
+	public void setImage75(Bitmap image75) {
+		this.image75 = image75;
+	}
+	public String getSpeakerBio() {
+		return speakerBio;
+	}
+	public void setSpeakerBio(String speakerBio) {
+		this.speakerBio = speakerBio;
+	}
+	
+	
+}
